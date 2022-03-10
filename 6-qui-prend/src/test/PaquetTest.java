d3b3ea9cf01362dcf7e3d83513008ac0a38a3ad7
iPrisc <florent.lelion@gmail.com>
2022-03-10 10:32:21 +0100

    Ajout de tests unitaires

---
+++ 6-qui-prend/src/test/PaquetTest.java
@@ -0,0 +1,48 @@
+package test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import game.Joueur;
+import game.Paquet;
+import game.Serie;
+
+public class PaquetTest {
+	
+	public PaquetTest() {
+	}
+	
+	@Test
+	public void TestPaquetComplet() {
+		// Teste si un paquet créé contient bien le bon nombre de carte initialement
+		Paquet  paquet = new Paquet();
+		assertTrue("Le nombre de carte intiale dans le paquet n'est pas correct !", paquet.nbCarte() == Paquet.NB_CARTE_PAQUET);
+	}
+
+	@Test
+	public void TestDistributionCarteJoueur() {
+		// Teste si la distribution des cartes à un joueur se fait correctement
+		Paquet  paquet = new Paquet();
+		Joueur joueur = new Joueur("Ringo");
+
+		int nbCartePaquetAvant = paquet.nbCarte();
+		paquet.distributionCartes(joueur);
+		int nbCartePaquetApres = paquet.nbCarte();
+		assertTrue("Le nombre de cartes restantes dans le paquet après distribution d'un joueur n'est pas correct !", nbCartePaquetAvant == nbCartePaquetApres + paquet.NB_CARTES_JOUEUR);
+	}
+
+	@Test
+	public void TestDistributionCarteSerie() {
+		// Teste si la distribution des cartes à une série se fait correctement
+		Paquet  paquet = new Paquet();
+		Serie serie = new Serie();
+
+		int nbCartePaquetAvant = paquet.nbCarte();
+		paquet.distributionSeries(serie);
+		int nbCartePaquetApres = paquet.nbCarte();
+		assertTrue("Le nombre de cartes restantes dans le paquet après distribution d'une série n'est pas correct !", nbCartePaquetAvant == nbCartePaquetApres +1);
+	}
+	
+}
---
+++ 6-qui-prend/src/test/SerieTest.java
@@ -0,0 +1,64 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import game.Carte;
+import game.Paquet;
+import game.Partie;
+import game.Serie;
+
+public class SerieTest {
+	
+	public SerieTest() {
+	}
+	
+	@Test
+	public void TestSeriePleine() {
+		// Teste si une serie auquel ont a ajouter le nombre maximal de carte est bien pleine
+		Serie serie = new Serie();
+		List<Carte> cartes = new ArrayList<Carte>();
+		for(int i=0; i<Serie.MAX_SERIE_CARTE; i++) {
+			Carte carte = new Carte();
+			serie.ajouterCarte(carte);
+			assertTrue("Le nombre de carte d'une serie ne correspond pas au nombre de carte ajoutée !", serie.nbCarte() == i+1);
+		}
+		assertTrue("Une serie auquel on a ajouter " + Serie.MAX_SERIE_CARTE + " n'est pas pleine !", serie.estPleine());
+	}
+
+	
+	@Test
+	public void TestDerniereCarte() {
+		// Teste si une serie auquel ont a ajouter le nombre maximal de carte est bien pleine
+		Serie serie = new Serie();
+		Carte carte1 = new Carte();
+		Carte carte2 = new Carte();
+		serie.ajouterCarte(carte1);
+		serie.ajouterCarte(carte2);
+		assertTrue("La dernière carte de la serie est celle que l'on a jouter en premier ! " , ! carte1.equals(serie.derniereCarte()));
+		assertTrue("La dernière carte de la serie n'est celle que l'on a jouter en dernier ! ", carte2.equals(serie.derniereCarte()));
+	}
+	
+
+	@Test
+	public void TestCarteEnTrop() {
+		// Teste si il n'est véritablement pas possible d'ajouter une carte à une série déjà pleine
+		Serie serie = new Serie();
+		List<Carte> cartes = new ArrayList<Carte>();
+		for(int i=0; i<Serie.MAX_SERIE_CARTE; i++) {
+			Carte carte = new Carte();
+			serie.ajouterCarte(carte);
+		}
+		int nbCarteserieAvant = serie.nbCarte();
+		Carte carteEnTrop = new Carte();
+		serie.ajouterCarte(carteEnTrop);
+		int nbCarteserieApres = serie.nbCarte();
+		
+		assertTrue("Une serie déjà pleine ne peut pas recevoir de nouvelle carte !", nbCarteserieAvant == nbCarteserieApres);
+	}
+
+}
